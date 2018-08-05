32024a1d6158a39dd89a41f478a08c40434f2678
Eugenia <38026810+Jenniely@users.noreply.github.com>
2018-08-04 22:09:02 -0500

    Update index.php

--- index.php
+++ index.php
@@ -1,11 +1,14 @@
 <?php
 session_start();
+
 require_once __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
+
 $loader = new Twig_Loader_Filesystem('./template');
 $twig = new Twig_Environment($loader, array('cache' => './tmp/cache', 'auto_reload' => true));
+
 $di = new Controller\Database($host, $database, $username, $password);
 $connection = $di->Connection(); 
+
 $r = new Controller\Router($connection, $twig);
-//var_dump($r);
 $r->route();
